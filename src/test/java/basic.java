7ee9a6b30776e147ac8e5fe40c4b4ae731b24a53
manjushgithub <manjushborse123@gmail.com>
2025-06-21 02:04:44 +0530

    first commit

---
+++ src/test/java/basic.java
@@ -0,0 +1,48 @@
+import files.payload;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+public class basic {
+    //save place id and then update the location or address and use get method to see daddress is update or not
+    public static void main(String[] args) {
+        RestAssured.baseURI="https://rahulshettyacademy.com";
+        String res=given().log().all().queryParam("key", "qaclick123").header("content-type","application/json")
+                .body(payload.addplace()).when().post("maps/api/place/add/json")
+                .then().assertThat().statusCode(200).body("scope",equalTo("APP")).
+                header("Server","Apache/2.4.52 (Ubuntu)").extract().response().asString();//this header as responce
+        //as we need place id for that we are extracting from the response
+        System.out.println(res);
+
+       // now this is like json then how we can extract values like place id from here
+        JsonPath js=new JsonPath(res);
+        String placeId=js.getString("place_id");
+        System.out.print(placeId);
+
+        //update the address
+        String resput = given().log().all().queryParam("key", "qaclick123")
+                .header("Content-Type", "application/json")
+                .body("{\n" +
+                        "\"place_id\":\"" + placeId + "\",\n" +
+                        "\"address\":\"70 Summer walk, USA\",\n" +
+                        "\"key\":\"qaclick123\"\n" +
+                        "}")
+                .when().put("maps/api/place/update/json")
+                .then().assertThat().statusCode(200)
+                .body("msg", equalTo("Address successfully updated")) // small case "successfully"
+                .extract().response().asString();
+
+        //validate that address is successfully updated or not
+
+        given().queryParam("key","qaclick123").queryParam("place_id",placeId).header("content-type","application/json").
+                when().get("maps/api/place/get/json")
+                .then().assertThat().body("address","70 Summer walk, USA");
+
+
+
+
+
+
+    }
+
+}
